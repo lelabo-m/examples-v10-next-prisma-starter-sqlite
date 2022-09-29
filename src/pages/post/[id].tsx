8e2bf35dca46cf32eae6aeca4a66734f7c4b13a6
lelabo_m <marc.lelabourier@gmail.com>
2022-09-29 16:16:20 +0200

    Bug example regarding createSSGProxy

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,7 +1,53 @@
+import { createProxySSGHelpers } from '@trpc/react/ssg';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { GetServerSidePropsContext } from 'next';
 import NextError from 'next/error';
 import { useRouter } from 'next/router';
+import superjson from 'superjson';
 import { NextPageWithLayout } from '~/pages/_app';
+import { createContext } from '~/server/context';
+import { appRouter } from '~/server/routers/_app';
 import { trpc } from '~/utils/trpc';
+
+async function createSSGProxy(opts: trpcNext.CreateNextContextOptions) {
+  return createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContext(opts),
+    transformer: superjson,
+  });
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ id: string }>,
+) {
+  const { req, res } = context;
+  const ssg = await createSSGProxy({
+    req,
+    res,
+  } as trpcNext.CreateNextContextOptions);
+
+  // This works...
+  // const ssg = createProxySSGHelpers({
+  //   router: appRouter,
+  //   ctx: await createContext({ req, res } as trpcNext.CreateNextContextOptions),
+  //   transformer: superjson,
+  // });
+
+  // This doesn't work...
+  const id = context.params?.id as string;
+  /*
+   * Prefetching the `post.byId` query here.
+   * `prefetch` does not return the result and never throws - if you need that behavior, use `fetch` instead.
+   */
+  await ssg.post.byId.prefetch({ id });
+  // Make sure to return { props: { trpcState: ssg.dehydrate() } }
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+}
 
 const PostViewPage: NextPageWithLayout = () => {
   const id = useRouter().query.id as string;
